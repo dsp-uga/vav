86ede9a825c60411e102b8ccac9e87524286a78c
Vyom Shrivastava <vyom.shrivastava1@gmail.com>
2018-04-27 14:54:15 -0400

    Added functionality to call preprocess

--- unet/main.py
+++ unet/main.py
@@ -2,10 +2,12 @@ import numpy as np
 import argparse
 from preprocessing import preprocess
 from fit_predict_unet import fit_predict
+from postprocessing import postprocess
 
 parser = argparse.ArgumentParser(description='paths')
 parser.add_argument('--mode',type=str,help='fit or predict?')
 parser.add_argument('--preprocess',type=str,help='True or False')
+parser.add_argument('--postprocess',type=str,help='True or False')
 parser.add_argument('--data_path',type=str,help='path of raw data folder')
 parser.add_argument('--image_path',type=str,help='path of image npy array')
 parser.add_argument('--mask_path',type=str,help='path of mask npy array')
@@ -17,6 +19,11 @@ if(args.preprocess=="True"):
     print("Preprocessing Images")
     preprocess(args.mode,args.data_path,args.save_path)
     print("Preprocessing Done!")
+
+elif(args.postprocess=="True"):
+    print("postprocessing Images")
+    postprocess(args.image_path,args.save_path)
+    print("Submission File saved as submission.csv at ",args.save_path)
 
 elif(args.mode=="fit"):
     print("Fitting Model")
